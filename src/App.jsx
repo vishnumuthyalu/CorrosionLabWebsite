c26336ede98cd866919dcfd4c924f9ff04c2babc
Vishnu Muthyalu <vm17college@gmail.com>
2026-02-03 11:26:36 -0600

    changed spelling

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Staff from './pages/Staff'
 import News from './pages/News'
 import STEMOutreach from './pages/STEMOutreach'
 import Achievements from './pages/Achievements'
-import Oppurtunities from './pages/Oppurtunities'
+import Opportunities from './pages/Opportunities'
 import './App.css'
 
 function App() {
@@ -29,7 +29,7 @@ function App() {
             <Route path="/news" element={<News />} />
             <Route path="/stem-outreach" element={<STEMOutreach />} />
             <Route path="/achievements" element={<Achievements />} />
-            <Route path="/oppurtunities" element={<Oppurtunities />} />
+            <Route path="/opportunities" element={<Opportunities />} />
           </Routes>
         </main>
         <Footer />
